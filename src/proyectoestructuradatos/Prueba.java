55b0a43fdd7bf0f70fb14367dde17236e1d8689b
Dylan <Dylan@DESKTOP-P654R6J>
2021-04-18 15:37:51 -0600

    actualizacion Proyecto

--- src/proyectoestructuradatos/Prueba.java
+++ src/proyectoestructuradatos/Prueba.java
@@ -9,15 +9,6 @@ package proyectoestructuradatos;
  *
  * @author Dylan
  */
-public class ProyectoEstructuraDatos {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        //pool
-        //error
-    }
+public class Prueba {
     
 }
